dcf92c5621d669c7f3a5fd2d1ead0f9e06e47f0c
Tina Hang <mstinahang@gmail.com>
2020-01-12 02:50:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
 // Write your code here
-let num1 = 2
-let num2 = 31
-let multiply = num1 * num2
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1
+
