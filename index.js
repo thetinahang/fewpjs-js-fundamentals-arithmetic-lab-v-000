94ef2f0e09640c3464c762536b53588a3b8545ec
Tina Hang <mstinahang@gmail.com>
2020-01-12 02:55:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 52 % 12;
+
+let max = Math.max(1,2,20);
